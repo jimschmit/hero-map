9e4ea43bc27466ab3b61cd817e3db5b46c99841b
Jim Schmit <contact@jim-schmit.dev>
2022-12-23 17:52:48 +0100

    add db backup

---
+++ sql/contacts_202212231752.sql
@@ -0,0 +1,30 @@
+INSERT INTO public.contacts (created_at,"name",phone_number,email,lat,lng,additional_info) VALUES
+	 ('2022-12-18 20:17:04.865479+01','Christine – 66125 Saarbrücken','0176 62167029',NULL,49.27891,7.0256467,'Curefip 17mg/ml --  >3Ampullen
+Hero 12mg -- 3x
+Hero 30mg -- 1x'),
+	 ('2022-12-18 21:23:53.783472+01','Dietmar Reichler','01749095018','kerott@freenet.de',49.43,11.0646,'90443 nürnberg
+GS CF und Aura 20 mg'),
+	 ('2022-12-18 19:53:17.410814+01','Petra Neumann ','0151 42366103','Petra.neumann73@hotmail.de ',49.88901,6.7905774,'PLZ 54523 '),
+	 ('2022-12-19 10:07:31.577122+01','Alexandra Haug','015738820062','Alex10580@web.de ',48.6357,9.33488,'Hero grün '),
+	 ('2022-12-19 14:46:42.712221+01','Karin ','0175 5421761 ','karin.sapper@t-online.de ',48.696243,10.2489805,'braune Hero'),
+	 ('2022-12-20 21:12:49.53819+01','Sonja','o1702410480','sonja.goertzen@ngs-systemberatung.de',50.667,10.446,'Bliss, Braune  Heros und Grüne  Heros'),
+	 ('2022-12-18 13:45:36.239856+01','Jim Schmit','+352 691902927 ','contact@jim-schmit.dev',49.8218,6.74718,'Hero Braun'),
+	 ('2022-12-21 14:29:13.042847+01','Beate',NULL,'Beatoll62@osnanet.de ',52.2974,7.41811,'Hero Braun
+PLZ 48431'),
+	 ('2022-12-21 14:56:49.566771+01','Carmen M. ','0176/41717525 ',NULL,48.9304,8.38797,'Aura 20, Curefip, Hero grün, Hero braun, PLZ 76275'),
+	 ('2022-12-21 17:17:19.196317+01','Marina ',' 01773750755','marina.grasl@gmail.com ',48.8792,12.5547,'braune Heros, BLISS Ampullen ');
+INSERT INTO public.contacts (created_at,"name",phone_number,email,lat,lng,additional_info) VALUES
+	 ('2022-12-21 18:48:56.24078+01','Janin','01733472066','Janin-pieritz@t-online.de',50.359608,10.275744,'Hero Braun und Grün'),
+	 ('2022-12-21 19:50:05.131782+01','Zehra',NULL,NULL,51.16569,10.451526,NULL),
+	 ('2022-12-22 21:00:24.523679+01','Tina','0172-5430460','tinahorn@freenet.de ',51.867348,10.559672,'Hero 12 & 30( grün & braun ) +  BlissV'),
+	 ('2022-12-22 22:02:30.133436+01','Ramona ','015253882481','ramona.mie@gmx.de',48.470486,11.93559,'85368'),
+	 ('2022-12-23 06:53:30.568181+01','Steffi','00 49 1590 2179617','steffi-f@freenet.de ',51.42099,14.242994,'HERO grün'),
+	 ('2022-12-23 08:49:07.754751+01','Susanne Grabenzeh ','017647355235','susannegrabenzeh@freenet.de',48.5764,9.78233,'89520 Heidenheim
+GS 20mg '),
+	 ('2022-12-23 14:18:56.91146+01','Ana','016095053953','ana_geraldo@yahoo.de',49.73881,10.034045,'11 Tabletten Hero braun'),
+	 ('2022-12-23 15:50:41.377316+01','Kerstin','01727919686',NULL,51.47427,11.969713,'braune Hero'),
+	 ('2022-12-23 17:08:05.762019+01','Ines S. ','01590 6355435 ','ines.siegler@gmx.de ',48.3296,8.05127,'Hero Braun, PLZ 77736 '),
+	 ('2022-12-23 17:19:41.923552+01','Melanie Christin','0178 8533921 ','info@handundpfote-lohfelden.de ',51.301468,9.5665455,'34253 Lohfelden ');
+INSERT INTO public.contacts (created_at,"name",phone_number,email,lat,lng,additional_info) VALUES
+	 ('2022-12-22 06:13:16.375513+01','Sandra  - 87466','015144339433','Wasa131081@gmail.com ',44.6836,6.7672,'Braune und grüne Hero
+PLZ 87466');
